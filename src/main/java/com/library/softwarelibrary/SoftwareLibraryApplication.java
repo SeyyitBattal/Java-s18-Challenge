620a1423e55a69bca4cd70793e92576e22aac750
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-27 11:28:36 +0300

    Project set up

---
+++ src/main/java/com/library/softwarelibrary/SoftwareLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.library.softwarelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/softwarelibrary/SoftwareLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.softwarelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
